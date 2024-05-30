1cbc16e8181a90a05efea0640ea43ad4d7c969ee
Devzio <Jasdevbedi@gmail.com>
2024-05-30 21:15:48 +1000

    Login and signup authentication logic

--- client/screens/LoginScreen.js
+++ client/screens/LoginScreen.js
@@ -1,23 +1,100 @@
 // screens/LoginScreen.js
-
-import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+// import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Image } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image } from 'react-native';
 import { GestureHandlerRootView, TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function LoginScreen({ navigation }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const users = JSON.parse(await AsyncStorage.getItem('users')) || [];
+        console.log('Users:', users);
+      } catch (error) {
+        console.error('Failed to fetch users', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setEmailError("Invalid email address");
+      return false;
+    }
+    setEmailError("");
+    return true;
+  };
+
+  const validatePassword = () => {
+    if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters long");
+      return false;
+    }
+    setPasswordError("");
+    return true;
+  };
+
+  const handleEmailChange = (text) => {
+    setEmailError("");
+    setEmail(text);
+  };
+
+  const handlePasswordChange = (text) => {
+    setPasswordError("");
+    setPassword(text);
+  };
+
+  // New loginUser function
+  const loginUser = async () => {
+    try {
+      // Get the array of users
+      const users = JSON.parse(await AsyncStorage.getItem('users')) || [];
+
+      // Check if a user with the entered email and password exists
+      const userExists = users.some(user => user.email === email && user.password === password);
+
+      if (userExists) {
+        console.log('User logged in successfully');
+        navigation.navigate('TabLayout'); //  Navigate to Home screen of the App
+      } else {
+        console.error('Invalid credentials');
+      }
+    } catch (error) {
+      console.error('Failed to log in', error);
+    }
+  };
 
   const handleLogin = () => {
-    // authentication logic here
+    const isEmailValid = validateEmail();
+    const isPasswordValid = validatePassword();
+    if (isEmailValid && isPasswordValid) {
+      loginUser(); // Call loginUser instead of logging to console
+      setEmail("");
+      setPassword("");
+    }
+  };
+
+  // Login for development purposes - bypasses authentication
+  const handleLoginDev = () => {
     navigation.navigate('TabLayout');
   };
 
-  const handleSignUp = () => {
+  const goToSignUp = () => {
     navigation.navigate('SignUp', { screen: 'SignupScreen' });
   };
+
+
 
   return (
     <GestureHandlerRootView>
@@ -27,7 +104,7 @@ export default function LoginScreen({ navigation }) {
 
         <View className="w-full h-full flex flex-column items-center">
           <View className="flex-column justify-center pt-16">
-            {/* Lightbulb */}
+            {/* Lightbulb image */}
             <Image
               style={styles.image}
               source={require('../assets/images/lightbulb.png')}>
@@ -41,57 +118,67 @@ export default function LoginScreen({ navigation }) {
           </View>
           {/* form */}
           <View className="w-full flex justify-around pt-5 pb-10 mt-auto bg-slate-200">
-            {/* form */}
             <View className="flex items-center mx-4 space-y-4 ">
+              <Text className="text-3xl text-slate-500 font-semibold mb-3">Login</Text>
+              {/* Email Input*/}
               <View className="bg-black/5 p-5 rounded-2xl w-full">
-                <TextInput placeholder='Email' placeholderTextColor={'gray'} />
+                <TextInput
+                  placeholder='Email'
+                  placeholderTextColor={'gray'}
+                  value={email}
+                  onChangeText={handleEmailChange}
+                  autoCapitalize="none"
+                />
               </View>
-              <View className="bg-black/5 p-5 rounded-2xl w-full mb-3">
-                <TextInput placeholder='Password' placeholderTextColor={'gray'} secureTextEntry />
+              {emailError ? <Text style={styles.error}>{emailError}</Text> : null}
+
+              {/* Password Input */}
+              <View className="bg-black/5 p-5 rounded-2xl w-full mb-3 flex-row justify-between">
+                <TextInput
+                  placeholder='Password'
+                  placeholderTextColor={'gray'}
+                  onChangeText={handlePasswordChange}
+                  value={password}
+                  secureTextEntry={!showPassword}
+                  autoCapitalize="none"
+                />
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => setShowPassword(!showPassword)}
+                >
+                  <Icon name={showPassword ? "eye" : "eye-slash"} size={20} color="gray" />
+                </TouchableOpacity>
               </View>
+              {passwordError ? (
+                <Text style={styles.error}>{passwordError}</Text>
+              ) : null}
+
+              {/* Login Button */}
               <View className='w-full'>
                 <TouchableOpacity
                   onPress={handleLogin}
                   className="w-full bg-[#12747c] p-3 rounded-2xl mb-3">
-                  <Text className="text-xl font-bold text-white text-center">Login</Text>
+                  <Text className="text-xl font-bold text-white text-center">
+                    Login
+                  </Text>
                 </TouchableOpacity>
+                {/* SignUp Button */}
+                <View className="flex-row justify-center">
+                  <Text>Don't hane an account? </Text>
+                  <TouchableOpacity onPress={goToSignUp}>
+                    <Text Text className="text-[#12747c] font-bold">
+                      Sign Up Here
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
-              <View className="flex-row justify-center">
-                <Text>Don't hanve an account? </Text>
-                <TouchableOpacity onPress={handleSignUp}>
-                  <Text Text className="text-[#12747c] font-bold">Sign Up Here</Text>
-                </TouchableOpacity>
-              </View>
+
+
             </View>
-
           </View>
-
         </View>
-
-
-
-
       </View>
-      {/* <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      /> */}
-
-      {/* <Button title="Login" onPress={handleLogin} /> */}
     </GestureHandlerRootView>
-
-
-
-
   );
 }
 
--- client/screens/SignupScreen.js
+++ client/screens/SignupScreen.js
@@ -1,18 +1,79 @@
 import React, { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
+// import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, TextInput, Button, StyleSheet, Image } from 'react-native';
 import { GestureHandlerRootView, TouchableOpacity } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function SignupScreen() {
-  const handleSignUp = () => {
-    navigation.navigate('SignUp', { screen: 'SignupScreen' });
+export default function SignupScreen({ navigation }) {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [showPassword, setShowPassword] = useState(false); // state variable to show/hide password input
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(user.email);
   };
+
+  const validatePassword = () => {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(user.password);
+  };
+
+  const handleSignUp = async () => {
+    try {
+      // Validate email and password
+      if (!validateEmail()) {
+        console.error("Invalid email address");
+        return;
+      }
+      if (!validatePassword()) {
+        console.warn(
+          "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        );
+        return;
+      }
+
+      // if (user.password !== confirmPassword) {
+      //   console.error("Passwords do not match");
+      //   return;
+      // }
+
+      // Get the current array of users
+      const currentUsers =
+        JSON.parse(await AsyncStorage.getItem("users")) || [];
+
+      // Check if the email is already registered
+      if (
+        currentUsers.some((currentUser) => currentUser.email === user.email)
+      ) {
+        console.error("Email is already registered");
+        return;
+      }
+
+      // Add the new user to the array
+      currentUsers.push(user);
+
+      // Save the updated array of users
+      await AsyncStorage.setItem("users", JSON.stringify(currentUsers));
+
+      console.log("User registered successfully");
+    } catch (error) {
+      console.error("Failed to register user", error);
+    }
+  };
+
   return (
     <GestureHandlerRootView>
       <View className="bg-slate-200 h-full w-full">
         <StatusBar style="light" />
-        <Image className="h-full w-full absolute" source={require('../assets/images/background.png')} />
+        <Image className="h-full w-full absolute" source={require('../assets/images/image.png')} />
 
         <View className="w-full h-full flex flex-column items-center">
           <View className="flex-column justify-center pt-16">
@@ -23,34 +84,63 @@ export default function SignupScreen() {
             </Image>
             {/* title */}
             <View className="flex items-center pt-3">
-              <Text className="text-white font-bold tracking-wider text-5xl mb-16">
+              <Text className="text-[#09c1bd] font-bold tracking-wider text-5xl mb-16">
                 ToDo App
               </Text>
             </View>
           </View>
           {/* form */}
-          <View className="w-full flex justify-around pt-5 pb-10 mt-auto">
+          <View className="w-full flex justify-around pt-5 pb-10 mt-auto bg-slate-200">
             {/* form */}
             <View className="flex items-center mx-4 space-y-4 ">
-              <Text className="text-3xl text-slate-500 font-semibold mb-5">Sign Up</Text>
+              <Text className="text-3xl text-slate-500 font-semibold mb-3">Sign Up</Text>
               <View className="bg-black/5 p-5 rounded-2xl w-full">
-                <TextInput placeholder='Email' placeholderTextColor={'gray'} />
+                <TextInput
+                  placeholder='Email'
+                  placeholderTextColor={'gray'}
+                  onChangeText={(text) => setUser({ ...user, email: text })}
+                  value={user.email}
+                  autoCapitalize="none"
+                />
               </View>
-              <View className="bg-black/5 p-5 rounded-2xl w-full mb-3">
-                <TextInput placeholder='Password' placeholderTextColor={'gray'} secureTextEntry />
+              <View className="bg-black/5 p-5 rounded-2xl w-full mb-3 flex-row justify-between">
+                <TextInput
+                  placeholder='Password'
+                  placeholderTextColor={'gray'}
+                  onChangeText={(text) => setUser({ ...user, password: text })}
+                  value={user.password}
+                  secureTextEntry={!showPassword}
+                  autoCapitalize="none"
+                />
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => setShowPassword(!showPassword)}
+                >
+                  <Icon name={showPassword ? "eye" : "eye-slash"} size={20} color="gray" />
+                </TouchableOpacity>
               </View>
+
               <View className='w-full'>
                 <TouchableOpacity
+                  // onPress={handleSignUp}
                   onPress={handleSignUp}
-                  className="w-full bg-sky-400 p-3 rounded-2xl mb-3">
+                  className="w-full bg-[#12747c] p-3 rounded-2xl mb-3">
                   <Text className="text-xl font-bold text-white text-center">Sign Up</Text>
                 </TouchableOpacity>
+
+                <View className="flex-row justify-center">
+                  <Text>Already Registered? </Text>
+                  <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Text Text className="text-[#12747c] font-bold">
+                      Login Here
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
 
             </View>
 
           </View>
-
         </View>
       </View>
     </GestureHandlerRootView>
@@ -80,5 +170,8 @@ const styles = StyleSheet.create({
     height: 150,
     objectFit: 'contain',
     resizeMode: 'contain'
+  },
+  buttonText: {
+    color: 'red'
   }
 });
